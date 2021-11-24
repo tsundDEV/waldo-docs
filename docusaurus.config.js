dda7b3f2012d43353460cad18c2f1203a079ad03
tsundere <benlmartin.work@gmail.com>
2021-11-24 10:42:09 +0000

    Setting base URL to /

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'WALDO Documentation',
   tagline: 'First visual anti-cheat powered by AI',
   url: 'https://tsundDev.github.io',
-  baseUrl: '/waldo-docs/',
+  baseUrl: '/',
   favicon: 'img/favicon.ico',
   organizationName: 'tsundDev', // Usually your GitHub org/user name.
   projectName: 'waldo-docs', // Usually your repo name.
